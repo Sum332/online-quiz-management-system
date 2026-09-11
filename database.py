6fae5814d0756d4ce46cfe2053a4742f39beae02
Sumit Jogi <sumitjogi641@gmail.com>
2026-09-11 21:49:14 +0530

    Add Aiven MySQL port configuration

--- database.py
+++ database.py
@@ -5,18 +5,10 @@ import mysql.connector
 def get_db_connection():
     connection = mysql.connector.connect(
         host=os.environ.get("DB_HOST", "localhost"),
+        port=int(os.environ.get("DB_PORT", 3306)),
         user=os.environ.get("DB_USER", "root"),
         password=os.environ.get("DB_PASSWORD"),
         database=os.environ.get("DB_NAME", "quiz_db")
     )
 
     return connection
-
-
-if __name__ == "__main__":
-    try:
-        db = get_db_connection()
-        print("Database connected successfully!")
-        db.close()
-    except Exception as e:
-        print("Database connection failed:", e)
